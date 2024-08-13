36b765ef4c91041d8827e9a9d22185c8e39256d8
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-08-13 15:35:48 +0200

    add navigation sidebar

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,8 +1,35 @@
 import "./navigation.css"
+import { NavLink } from 'react-router-dom'
 
 const Navigation = () => {
     return (
         <aside>
+            <ul className="navigation-ul">
+                <li>
+                    <NavLink to={'/'}>
+                        <img src="../../src/assets/home-icon.svg" alt="Home icon" />
+                        <p className="home-text">Home</p>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/stats'}>
+                        <img src="../../src/assets/notes-icon.svg" alt="Notes icon" />
+                        <p>Stats</p>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/profile'}>
+                        <img src="../../src/assets/profile-icon.svg" alt="Profile icon" />
+                        <p>Profile</p>
+                    </NavLink>
+                </li>
+            </ul>
+            <div className="about-container">
+                <NavLink to={'/about'}>
+                <img src="../../src/assets/info-icon.svg" alt="Information icon" />
+                <p>About</p>
+            </NavLink>
+            </div>
 
         </aside>
     )
