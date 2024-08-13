e671d9f499107d11437a5f832e9b34bb15534b0e
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-08-13 14:12:09 +0200

    small css changes

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -20,48 +20,50 @@ const Login = () => {
     }
 
     return (
-        <div className="login-form-container">
-            <div className="logo-container">
-                <img src="/../../src/assets/logo.png" alt="logo" className="logo"/>
-            </div>
-            
-            <div className="login-form">
-                <h2>Login</h2>
-                <Form>
-                        <TextInput
-                            value={formData.email}
-                            onChange={onChange}
-                            name="email"
-                            label={"Email *"}
-                        />
-                        <TextInput
-                            value={formData.password}
-                            onChange={onChange}
-                            name="password"
-                            label={"Password *"}
-                            type={"password"}
-                        />
-                </Form>
-
-                <div className="error-message-container">
-                    {error && <p className="error-message">{error}</p>}
+        <div className="login-register-container">
+            <div className="login-form-container">
+                <div className="logo-container">
+                    <img src="/../../src/assets/logo.png" alt="logo" className="logo"/>
                 </div>
+                
+                <div className="login-form">
+                    <h2>Login</h2>
+                    <Form>
+                            <TextInput
+                                value={formData.email}
+                                onChange={onChange}
+                                name="email"
+                                label={"Email *"}
+                            />
+                            <TextInput
+                                value={formData.password}
+                                onChange={onChange}
+                                name="password"
+                                label={"Password *"}
+                                type={"password"}
+                            />
+                    </Form>
 
-                <div className="login-button-container">
-                    <Button
-                        text="Log in"
-                        onClick={() => handleLogin(formData.email, formData.password)}
-                        classes="login-button"
-                    />
-                </div>
+                    <div className="error-message-container">
+                        {error && <p className="error-message">{error}</p>}
+                    </div>
 
-                <div className="need-account-container">
-                    <p>Need an account?</p>
-                    <Button
-                        text="Register"
-                        onClick={() => navigate('/register')}
-                        classes="register-button"
-                    />
+                    <div className="login-button-container">
+                        <Button
+                            text="Log in"
+                            onClick={() => handleLogin(formData.email, formData.password)}
+                            classes="login-button"
+                        />
+                    </div>
+
+                    <div className="need-account-container">
+                        <p>Need an account?</p>
+                        <Button
+                            text="Register"
+                            onClick={() => navigate('/register')}
+                            classes="register-button"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -25,59 +25,63 @@ const Register = () => {
     }
 
     return (
-        <div className="register-form-container">
-            <div className="logo-container">
-                <img src="/../../src/assets/logo.png" alt="logo" className="logo"/>
-            </div>
-            <div className="register-form">
-                <h2>Register</h2>
-                <Form>
-                        <TextInput
-                            value={formData.email}
-                            onChange={onChange}
-                            name="email"
-                            label={"Email *"}
-                        />
-                        <TextInput
-                            value={formData.password}
-                            onChange={onChange}
-                            name="password"
-                            label={"Password *"}
-                            type={"password"}
-                        />
-                        <TextInput
-                            value={formData.first_name}
-                            onChange={onChange}
-                            name="first_name"
-                            label={"First name *"}
-                        />
-                        <TextInput
-                            value={formData.last_name}
-                            onChange={onChange}
-                            name="last_name"
-                            label={"Last name *"}
-                        />
-                </Form>
-
-                <div className="error-message-container">
-                    {error && <p className="error-message">{error}</p>}
+        <div className="login-register-container ">
+            <div className="register-form-container">
+                <div className="logo-container">
+                    <img src="/../../src/assets/logo.png" alt="logo" className="logo"/>
                 </div>
 
-                <div className="login-button-container">
-                    <Button
-                        text="Register"
-                        onClick={() => handleRegister(formData.email, formData.password, formData.first_name, formData.last_name)}
-                        classes="login-button"
-                    />
-                </div>
+                <div className="register-form">
+                    <h2>Register</h2>
 
-                <div className="need-account-container">
-                    <p>Already an account?</p>
-                    <Button
-                        text="Login"
-                        onClick={() => navigate('/login')}
-                        classes="register-button"
-                    />
+                    <Form>
+                            <TextInput
+                                value={formData.email}
+                                onChange={onChange}
+                                name="email"
+                                label={"Email *"}
+                            />
+                            <TextInput
+                                value={formData.password}
+                                onChange={onChange}
+                                name="password"
+                                label={"Password *"}
+                                type={"password"}
+                            />
+                            <TextInput
+                                value={formData.first_name}
+                                onChange={onChange}
+                                name="first_name"
+                                label={"First name *"}
+                            />
+                            <TextInput
+                                value={formData.last_name}
+                                onChange={onChange}
+                                name="last_name"
+                                label={"Last name *"}
+                            />
+                    </Form>
+
+                    <div className="error-message-container">
+                        {error && <p className="error-message">{error}</p>}
+                    </div>
+
+                    <div className="login-button-container">
+                        <Button
+                            text="Register"
+                            onClick={() => handleRegister(formData.email, formData.password, formData.first_name, formData.last_name)}
+                            classes="login-button"
+                        />
+                    </div>
+
+                    <div className="need-account-container">
+                        <p>Already an account?</p>
+                        <Button
+                            text="Login"
+                            onClick={() => navigate('/login')}
+                            classes="register-button"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
